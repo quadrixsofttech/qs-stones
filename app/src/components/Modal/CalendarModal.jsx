4267a6d7b49f4070ba6adadbba0cb1a7583693c4
bonderovicmartin8 <131369712+bonderovicmartin8@users.noreply.github.com>
2023-05-11 11:55:55 +0200

    Cleaned up the code

--- app/src/components/Modal/CalendarModal.jsx
+++ app/src/components/Modal/CalendarModal.jsx
@@ -1,96 +1,3 @@
-// import {
-//   Box,
-//   Divider,
-//   Flex,
-//   Progress,
-//   Select,
-//   Tag,
-//   TagCloseButton,
-//   TagLabel,
-//   Text,
-//   Tooltip,
-// } from '@chakra-ui/react';
-// import { Info } from '@material-ui/icons';
-// import styles from './CalendarModal.styles';
-// import { useCalendar } from '../../hooks/useCalendar';
-// import { CustomCalendar } from '../CustomCalendar/CustomCalendar';
-
-// export const CalendarModal = (props) => {
-//   const { handleCalendarChange, handleRemoveTag, tagArray, selectedRange } =
-//     useCalendar();
-//   return (
-//     <>
-//       <Flex {...styles.progress}>
-//         <Box>1/2</Box>
-//         <Progress hasStripe value={props.value} flex={1} colorScheme="purple" />
-//       </Flex>
-//       <Box position="relative" zIndex={999999}>
-//         <Flex alignItems="center" mt={2} gap={2}>
-//           <Text {...styles.textRemote}>{props.name}</Text>
-//           <Tooltip
-//             label="*Double-click to select a date on the calendar.
-//                   *Single-click to select a range of dates on the calendar."
-//             placement="right"
-//             hasArrow
-//           >
-//             <Info fontSize={'small'} style={{ color: '#A0AEC0' }} />
-//           </Tooltip>
-//         </Flex>
-//       </Box>
-//       <Select mt={2}>
-//         <option value="option1">Milos Stosic(ADMIN)</option>
-//         <option value="option2">Igor Stosic(ADMIN)</option>
-//       </Select>
-//       <CustomCalendar />
-//       {props.isClicked ?? (
-//         <>
-//           <Text {...styles.textRequestDates}>
-//             Requested dates for Vacation:
-//           </Text>
-//           <Flex {...styles.flexTag}>
-//             {tagArray?.map((tag, index) => (
-//               <Tag
-//                 key={index}
-//                 size={'sm'}
-//                 fontSize={'12px'}
-//                 borderRadius="full"
-//                 variant="subtle"
-//                 colorScheme={tag.color}
-//               >
-//                 <Tooltip label={tag.label} placement="bottom" hasArrow>
-//                   <TagLabel>{tag.label}</TagLabel>
-//                 </Tooltip>
-//                 <TagCloseButton onClick={() => handleRemoveTag(index)} />
-//               </Tag>
-//             ))}
-//           </Flex>
-//           <Divider />
-//         </>
-//       )}
-//       <Divider />
-//       <Text {...styles.textRequestDates}>Requested dates for Remote:</Text>
-//       <Flex {...styles.flexTag}>
-//         {tagArray?.map((tag, index) => (
-//           <Tag
-//             key={index}
-//             size={'sm'}
-//             fontSize={'12px'}
-//             borderRadius="full"
-//             variant="subtle"
-//             colorScheme={tag.color}
-//           >
-//             <Tooltip label={tag.label} placement="bottom" hasArrow>
-//               <TagLabel>{tag.label}</TagLabel>
-//             </Tooltip>
-//             <TagCloseButton onClick={() => handleRemoveTag(index)} />
-//           </Tag>
-//         ))}
-//       </Flex>
-//       <Divider />
-//     </>
-//   );
-// };
-
 import {
   Box,
   Divider,
@@ -106,17 +13,24 @@ import {
 import { Info } from '@material-ui/icons';
 import styles from './CalendarModal.styles';
 import { useCalendar } from '../../hooks/useCalendar';
-import { CustomCalendar } from '../CustomCalendar/CustomCalendar';
+import { Calendar } from 'react-multi-date-picker';
+import { useState } from 'react';
 
 export const CalendarModal = (props) => {
-  const { handleRemoveTag, tagArray,handleCalendarChange } = useCalendar();
+  const [remoteValues, setRemoteValues] = useState([]);
+  const { tagArray, handleRemoveTag } = useCalendar();
+
+  const handleOnChange = (value) => {
+    setRemoteValues(value);
+  };
+
   return (
     <>
       <Flex {...styles.progress}>
         <Box>1/2</Box>
         <Progress hasStripe value={props.value} flex={1} colorScheme="purple" />
       </Flex>
-      <Box position="relative" zIndex={999999}>
+      <Box position="relative">
         <Flex alignItems="center" mt={2} gap={2}>
           <Text {...styles.textRemote}>{props.name}</Text>
           <Tooltip
@@ -133,40 +47,47 @@ export const CalendarModal = (props) => {
         <option value="option1">Milos Stosic(ADMIN)</option>
         <option value="option2">Igor Stosic(ADMIN)</option>
       </Select>
-      <CustomCalendar />
+      <Flex alignItems={'center'} justifyContent={'center'}>
+        <Calendar
+          range
+          rangeHover
+          multiple
+          numberOfMonths={2}
+          value={remoteValues}
+          onChange={handleOnChange}
+        />
+      </Flex>
       {props.isClicked ?? (
         <>
           <Text {...styles.textRequestDates}>
             Requested dates for Vacation:
           </Text>
-          {console.log('array',tagArray)}
-          {tagArray && tagArray.length > 0 ? (
-            <Flex {...styles.flexTag}>
-              {tagArray.map((tag, index) => (
-                <Tag
-                  key={index}
-                  size={'sm'}
-                  fontSize={'12px'}
-                  borderRadius="full"
-                  variant="subtle"
-                  colorScheme={tag.color}
-                >
-                  <Tooltip label={tag.label} placement="bottom" hasArrow>
-                    <TagLabel>{tag.label}</TagLabel>
-                  </Tooltip>
-                  <TagCloseButton onClick={() => handleRemoveTag(index)} />
-                </Tag>
-              ))}
-            </Flex>
-          ) : null}
+
+          <Flex>
+            {tagArray.map((tag, index) => (
+              <Tag
+                key={index}
+                size={'sm'}
+                fontSize={'12px'}
+                borderRadius="full"
+                variant="subtle"
+                colorScheme={tag.color}
+              >
+                <Tooltip label={tag.label} placement="bottom" hasArrow>
+                  <TagLabel>{tag.label}</TagLabel>
+                </Tooltip>
+                <TagCloseButton onClick={() => handleRemoveTag(index)} />
+              </Tag>
+            ))}
+          </Flex>
           <Divider />
         </>
       )}
       <Divider />
       <Text {...styles.textRequestDates}>Requested dates for Remote:</Text>
       <Flex {...styles.flexTag}>
-        {console.log('dadad')}
-        {tagArray.map((tag, index) => {
+        {/* {console.log('dadad')} */}
+        {/* {tagArray.map((tag, index) => {
           console.log('daddadaddadaddadaer554ada');
           return (
             <Tag
@@ -186,7 +107,7 @@ export const CalendarModal = (props) => {
               <TagCloseButton onClick={() => handleRemoveTag(index)} />
             </Tag>
           );
-        })}
+        })} */}
       </Flex>
       <Divider />
     </>
--- app/src/hooks/useCalendar.js
+++ app/src/hooks/useCalendar.js
@@ -1,121 +1,26 @@
-  // const handleCalendarChange = (nextSelectedRange) => {
-  //   setSelectedRange(nextSelectedRange);
-
-  //   if (nextSelectedRange.length === 1) {
-  //     const selectedDate = new Date(nextSelectedRange[0]);
-  //     setStartDate(selectedDate);
-  //     setEndDate(selectedDate);
-  //     return;
-  //   }
-
-  //   const start = new Date(nextSelectedRange[0]);
-  //   const end = new Date(nextSelectedRange[1]);
-  //   const datesArray = [];
-  //   let currentDate = new Date(start);
-
-  //   while (currentDate <= end) {
-  //     datesArray.push(new Date(currentDate));
-  //     currentDate.setDate(currentDate.getDate() + 1);
-  //   }
-
-  //   const tagLabel = `${start.toLocaleDateString()} - ${end.toLocaleDateString()}`;
-  //   const tagColor = 'gray';
-  //   const newTag = {
-  //     label: tagLabel,
-  //     color: tagColor,
-  //     startDate: start,
-  //     endDate: end,
-  //     dates: datesArray,
-  //   };
-
-  //   setTagArray((prevTagArray) => [...prevTagArray, newTag]);
-  //   setStartDate(start);
-  //   setEndDate(end);
-
-  //   console.log(tagArray);
-  // };
-
-  // const handleCalendarChange = (nextSelectedRange) => {
-  //   setSelectedRange(nextSelectedRange);
-
-  //   const start = new Date(nextSelectedRange[0]);
-  //   const end = new Date(nextSelectedRange[1]);
-  //   const datesArray = [];
-  //   let currentDate = new Date(start);
-
-  //   while (currentDate <= end) {
-  //     datesArray.push(new Date(currentDate));
-  //     currentDate.setDate(currentDate.getDate() + 1);
-  //   }
-
-  //   const filteredDatesArray = datesArray.filter((date) =>
-  //     nextSelectedRange.includes(date.toISOString())
-  //   );
-
-  //   const tagLabel = `${start.toLocaleDateString()} - ${end.toLocaleDateString()}`;
-  //   const tagColor = 'gray';
-  //   const newTag = {
-  //     label: tagLabel,
-  //     color: tagColor,
-  //     startDate: start,
-  //     endDate: end,
-  //     dates: filteredDatesArray,
-  //   };
-
-  //   setTagArray((prevTagArray) => [...prevTagArray, newTag]);
-  //   console.log(tagArray);
-  // };
-
 import { useDisclosure, useToast } from '@chakra-ui/react';
-import {  useState } from 'react';
+import { useState } from 'react';
+import moment from 'moment';
 
 export const useCalendar = () => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [selectedRange, setSelectedRange] = useState([]);
   const [tagArray, setTagArray] = useState([]);
   const { onClose } = useDisclosure();
 
   const toast = useToast();
 
-
-  const handleCalendarChange = (nextSelectedRange) => {
-    setSelectedRange(nextSelectedRange);
-  
-    const start = new Date(nextSelectedRange[0]);
-    const end = new Date(nextSelectedRange[nextSelectedRange.length - 1]);
-  
-    const datesArray = [];
-    let currentDate = new Date(start);
-  
-    while (currentDate <= end) {
-      datesArray.push(new Date(currentDate));
-      currentDate.setDate(currentDate.getDate() + 1);
-    }
-  
-    const filteredDatesArray = datesArray.filter((date) =>
-      nextSelectedRange.some(
-        (selectedDate) => new Date(selectedDate).getTime() === date.getTime()
-      )
-    );
-  
-    const tagLabel = `${start.toLocaleDateString()} - ${end.toLocaleDateString()}`;
+  const handleTag = (start, end) => {
+    const tagLabel = `${moment(start).format('YYYY-MM-DD')} - ${moment(end).format('YYYY-MM-DD')}`;
     const tagColor = 'gray';
     const newTag = {
       label: tagLabel,
       color: tagColor,
-      startDate: start,
-      endDate: end,
-      dates: filteredDatesArray,
+      startDate: moment(start).toDate(),
+      endDate: moment(end).toDate(),
     };
   
     setTagArray((prevTagArray) => [...prevTagArray, newTag]);
-    setStartDate(null);
-    setEndDate(null);
-    console.log(tagArray)
+    console.log(tagArray);
   };
-
-
   const handleRemoveTag = (tagIndex) => {
     const updatedTagArray = [...tagArray];
     updatedTagArray.splice(tagIndex, 1);
@@ -127,7 +32,7 @@ export const useCalendar = () => {
     return toast({
       title: 'Success',
       description:
-        'Success You have submitted a request to the Admin for scheduiling vacation and remote work',
+        'Success You have submitted a request to the Admin for scheduling vacation and remote work',
       position: 'top',
       status: 'success',
       isClosable: false,
@@ -137,15 +42,10 @@ export const useCalendar = () => {
   };
 
   return {
-    startDate,
-    endDate,
-    selectedRange,
     tagArray,
-    handleCalendarChange,
     handleRemoveTag,
     handleSubmit,
     setTagArray,
-    setStartDate,
-    setEndDate
+    handleTag
   };
 };
