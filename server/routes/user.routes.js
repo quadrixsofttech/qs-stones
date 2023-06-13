6ebb281730c32d81aed060242f96b83774461702
bonderovicmartin8 <131369712+bonderovicmartin8@users.noreply.github.com>
2023-06-13 15:36:51 +0200

    Cleaned up user.routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,40 +1,11 @@
 const express = require('express');
+const { requireAuth } = require('../middleware/user');
+const { getUser, updateUserRole } = require('../controllers/user.controller');
+
 const router = express.Router();
 
-const User = require('../data/User');
-const { requireAuth } = require('../middleware/user');
+router.get('/users', requireAuth, getUser);
 
-router.get('/users', requireAuth, async (req, res) => {
-  try {
-    const users = await User.find()
-      .lean()
-      .select('_id firstName lastName avatar email');
-
-    res.json({
-      users,
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: 'There was a problem getting the users',
-    });
-  }
-});
-
-router.patch('/user-role', async (req, res) => {
-  try {
-    const { role } = req.body;
-    const allowedRoles = ['user', 'admin'];
-
-    if (!allowedRoles.includes(role)) {
-      return res.status(400).json({ message: 'Role not allowed' });
-    }
-    await User.findOneAndUpdate({ _id: req.user.sub }, { role });
-    res.json({
-      message: 'User role updated.',
-    });
-  } catch (err) {
-    return res.status(400).json({ error: err });
-  }
-});
+router.patch('/user-role', updateUserRole);
 
 module.exports = router;
