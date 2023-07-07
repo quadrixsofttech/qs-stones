aaa8cdad5a3de4dd737b055a806b362978a92bc5
ivan-srejic <ivan.srejic@quadrixsoft.com>
2023-07-07 12:09:22 +0200

    Timeline Component refactored

--- app/src/components/Timeline/Timeline.jsx
+++ app/src/components/Timeline/Timeline.jsx
@@ -1,37 +1,15 @@
 import TimelineHorizontal from './TimelineHorizontal/TimelineHorizontal';
 import TimelineVertical from './TimelineVertical/TimelineVertical';
 
-const Timeline = ({
-  type = 'vertical',
-  data,
-  title,
-  startHour,
-  endHour,
-  onOpen,
-  onEdit,
-  onDelete,
-}) => {
-  return type === 'horizontal' ? (
-    <TimelineHorizontal
-      data={data}
-      title={title}
-      startHour={startHour}
-      endHour={endHour}
-      onOpen={onOpen}
-      onEdit={onEdit}
-      onDelete={onDelete}
-    />
-  ) : (
-    <TimelineVertical
-      data={data}
-      title={title}
-      startHour={startHour}
-      endHour={endHour}
-      onOpen={onOpen}
-      onEdit={onEdit}
-      onDelete={onDelete}
-    />
-  );
+const Timeline = ({ type = 'vertical', ...rest }) => {
+  const TimelineComponents = {
+    horizontal: TimelineHorizontal,
+    vertical: TimelineVertical,
+  };
+
+  const TimelineComponent = TimelineComponents[type];
+
+  return <TimelineComponent {...rest} />;
 };
 
 export default Timeline;
