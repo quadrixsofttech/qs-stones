0c8c3134d4c31b96824b65540305c1dbaa3019b6
ivan-srejic <ivan.srejic@quadrixsoft.com>
2023-07-07 15:56:36 +0200

    Conference room route,controller and model impleme

---
+++ server/services/conference-room/models/conference-room.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ConferenceRoomSchema = new Schema({
+  roomNumber: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  equipment: [
+    {
+      type: {
+        type: String,
+        enum: ['laptop', 'chalkboard', 'tv', 'wifi'],
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  img: {
+    type: String,
+    required: true,
+  },
+  floor: {
+    type: String,
+    enum: ['Upper Floor', 'Lower Floor'],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model(
+  'conference-room-overview',
+  ConferenceRoomSchema
+);
