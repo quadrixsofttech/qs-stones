8c9cf7b9f343f6a6a05e8959fb5c9a384906bce7
ivan-srejic <ivan.srejic@quadrixsoft.com>
2023-07-12 11:01:52 +0200

    Revert "Merge branch 'MARTIN_BR_MVC_#1'"
    This reverts commit 8e7816c7a8914332a7fb46fce9901c17aec36373, reversing
    changes made to e6d5a10b42a48f6ed7b224173e491af772046b57.

--- server/routes/csrf-token.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  return res.json({ csrfToken: req.csrfToken(), token: req.cookies.token });
-});
-
-module.exports = router;
--- server/routes/dashboard.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { requireAuth } = require('../middleware/user');
-
-router.get('/', requireAuth, (req, res) => res.json(dashboardData));
-
-module.exports = router;
