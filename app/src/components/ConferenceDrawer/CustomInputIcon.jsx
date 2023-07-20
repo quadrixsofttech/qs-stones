df642a8f33fe6ca26dd7bc60c7463ae262ff0497
bonderovicmartin8 <131369712+bonderovicmartin8@users.noreply.github.com>
2023-07-20 12:20:04 +0200

    Deleted unnecesery files

--- app/src/components/ConferenceDrawer/CustomInputIcon.jsx
+++
@@ -1,7 +0,0 @@
-import { Icon } from '@chakra-ui/react';
-import React from 'react';
-import { AiTwotoneCalendar } from 'react-icons/ai';
-
-export default function CustomInputIcon(props) {
-  return <Icon as={AiTwotoneCalendar} {...props} />;
-}
