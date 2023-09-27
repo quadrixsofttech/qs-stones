d5d044f2df0b63d3ffd2f69b2af748a2e9640827
bonderovicmartin8 <131369712+bonderovicmartin8@users.noreply.github.com>
2023-09-27 12:32:55 +0200

    clean start

--- server/routes/public.routes.js
+++ server/routes/public.routes.js
@@ -8,7 +8,6 @@ const dashboardRouter = require('./dashboard.routes');
 const bioRouter = require('./bio.routes');
 const ptoRouter = require('./pto.routes');
 const conferenceRoomRouter = require('./conference-room.routes');
-const reserveConference = require('./reserve-conference.routes');
 const { attachUser } = require('../middleware/user');
 
 const router = express.Router();
@@ -24,6 +23,5 @@ router.use('/bio', bioRouter);
 router.use('/inventory', inventoryRouter);
 router.use('/paid-time-off', ptoRouter);
 router.use('/conference-rooms', conferenceRoomRouter);
-router.use('/reserve-conference', reserveConference);
 
 module.exports = router;
--- server/routes/reserve-conference.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ReserveConferenceController = require('../controllers/reserve-conference.controller');
-
-router.post('/', ReserveConferenceController.createReservationController);
-router.get('/', ReserveConferenceController.getReservationsController);
-
-module.exports = router;
