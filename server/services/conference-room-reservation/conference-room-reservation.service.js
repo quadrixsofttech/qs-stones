76bd6c4c7b9227497322c9f3a399eaca9dd1e9da
ivan-srejic <ivan.srejic@quadrixsoft.com>
2023-09-28 16:16:26 +0200

    getReservation refactored

--- server/services/conference-room-reservation/conference-room-reservation.service.js
+++ server/services/conference-room-reservation/conference-room-reservation.service.js
@@ -1,3 +1,4 @@
+const conferenceRoom = require('../../models/conference-room');
 const ConferenceRoomReservation = require('../../models/conference-room-reservation');
 
 const createReservation = async ({
@@ -32,62 +33,86 @@ const createReservation = async ({
 
 const getReservations = async (date) => {
   try {
-    const reservations = await ConferenceRoomReservation.aggregate([
-      {
-        $match: {
-          date: new Date(date),
-        },
+    // const reservations = await ConferenceRoomReservation.aggregate([
+    //   {
+    //     $match: {
+    //       date: new Date(date),
+    //     },
+    //   },
+    //   {
+    //     $lookup: {
+    //       from: 'users',
+    //       let: { userId: '$userId' },
+    //       pipeline: [
+    //         {
+    //           $match: {
+    //             $expr: { $eq: ['$_id', '$$userId'] },
+    //           },
+    //         },
+    //         {
+    //           $project: {
+    //             _id: 0,
+    //             firstName: 1,
+    //             lastName: 1,
+    //             src: 1,
+    //           },
+    //         },
+    //       ],
+    //       as: 'user',
+    //     },
+    //   },
+    //   {
+    //     $addFields: {
+    //       user: { $arrayElemAt: ['$user', 0] },
+    //     },
+    //   },
+    //   {
+    //     $lookup: {
+    //       from: 'conference-room-overviews',
+    //       let: { conferenceRoom: '$conferenceRoom' },
+    //       pipeline: [
+    //         {
+    //           $match: {
+    //             $expr: { $eq: ['$_id', '$$conferenceRoom'] },
+    //           },
+    //         },
+    //       ],
+    //       as: 'conferenceRoom',
+    //     },
+    //   },
+    //   {
+    //     $addFields: {
+    //       conferenceRoom: { $arrayElemAt: ['$conferenceRoom', 0] },
+    //       column: { $arrayElemAt: ['$conferenceRoom.name', 0] },
+    //       floor: { $arrayElemAt: ['$conferenceRoom.floor', 0] },
+    //       id: { $arrayElemAt: ['$conferenceRoom.id', 0] },
+    //     },
+    //   },
+    // ]);
+
+    const reservationsFull = await ConferenceRoomReservation.find({
+      date: date,
+    })
+      .populate({ path: 'userId', select: '_id firstName lastName' })
+      .populate('conferenceRoom');
+
+    const reservations = reservationsFull.map((reservation) => ({
+      _id: reservation._id,
+      userId: reservation.userId._id,
+      date: reservation.date,
+      startTime: reservation.startTime,
+      endTime: reservation.endTime,
+      title: reservation.title,
+      description: reservation.description,
+      color: reservation.color,
+      floor: reservation.conferenceRoom.floor,
+      column: reservation.conferenceRoom.name,
+      id: reservation.conferenceRoom.id,
+      user: {
+        firstName: reservation.userId.firstName,
+        lastName: reservation.userId.lastName,
       },
-      {
-        $lookup: {
-          from: 'users',
-          let: { userId: '$userId' },
-          pipeline: [
-            {
-              $match: {
-                $expr: { $eq: ['$_id', '$$userId'] },
-              },
-            },
-            {
-              $project: {
-                _id: 0,
-                firstName: 1,
-                lastName: 1,
-                src: 1,
-              },
-            },
-          ],
-          as: 'user',
-        },
-      },
-      {
-        $addFields: {
-          user: { $arrayElemAt: ['$user', 0] },
-        },
-      },
-      {
-        $lookup: {
-          from: 'conference-room-overviews',
-          let: { conferenceRoom: '$conferenceRoom' },
-          pipeline: [
-            {
-              $match: {
-                $expr: { $eq: ['$_id', '$$conferenceRoom'] },
-              },
-            },
-          ],
-          as: 'conferenceRoom',
-        },
-      },
-      {
-        $addFields: {
-          conferenceRoom: { $arrayElemAt: ['$conferenceRoom', 0] },
-          column: { $arrayElemAt: ['$conferenceRoom.name', 0] },
-          floor: { $arrayElemAt: ['$conferenceRoom.floor', 0] },
-          id: { $arrayElemAt: ['$conferenceRoom.id', 0] },
-        },
-      },
-    ]);
+    }));
 
     return reservations;
   } catch (err) {
