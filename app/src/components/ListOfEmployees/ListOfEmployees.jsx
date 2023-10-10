14b51d4be6cceb1f6a197b2c6fc274c41d30e30e
ivan-srejic <ivan.srejic@quadrixsoft.com>
2023-10-10 10:57:46 +0200

    Click and hover functionality implemented

--- app/src/components/ListOfEmployees/ListOfEmployees.jsx
+++ app/src/components/ListOfEmployees/ListOfEmployees.jsx
@@ -9,12 +9,33 @@ import {
   Text,
   Tr,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ListOfEmployees.styles';
 import { BiRightArrowAlt } from 'react-icons/bi';
 import NotificationIcon from './notificationIcon';
+import { useTheme } from '@emotion/react';
 
 const ListOfEmployees = () => {
+  const [hoveredRowIndex, setHoveredRowIndex] = useState(null);
+  const [clickedRowIndex, setClickedRowIndex] = useState(null);
+
+  const handleRowClick = (rowIndex) => {
+    if (rowIndex === clickedRowIndex) {
+      setClickedRowIndex(null);
+    } else {
+      setClickedRowIndex(rowIndex);
+    }
+  };
+
+  const employeeData = [
+    { name: 'Ivan Srejic' },
+    { name: 'Martini Martini' },
+    { name: 'Pera Peric' },
+  ];
+
+  const theme = useTheme();
+  const purple400 = theme.colors.purple[400];
+
   return (
     <Flex {...styles.mainBox}>
       <Heading as="h2" {...styles.header}>
@@ -23,20 +44,45 @@ const ListOfEmployees = () => {
       <TableContainer overflowY={'auto'}>
         <Table variant="simple">
           <Tbody>
-            <Tr>
-              <Td width={'56px'}>
-                <Avatar size={'xs'} />
-              </Td>
-              <Td>
-                <Text>Ivan Srejic</Text>
-              </Td>
-              <Td>
-                <Flex {...styles.iconBox}>
-                  <NotificationIcon number="1" />
-                  <BiRightArrowAlt size="20" />
-                </Flex>
-              </Td>
-            </Tr>
+            {employeeData.map((employee, index) => (
+              <Tr
+                key={index}
+                onMouseEnter={() => setHoveredRowIndex(index)}
+                onMouseLeave={() => setHoveredRowIndex(null)}
+                backgroundColor={index === clickedRowIndex ? 'gray.100' : ''}
+                _hover={
+                  hoveredRowIndex === index
+                    ? { backgroundColor: 'gray.100' }
+                    : 'null'
+                }
+                boxShadow={
+                  index === clickedRowIndex
+                    ? `inset 0px 0px 0px 1px ${purple400}`
+                    : ''
+                }
+                onClick={() => handleRowClick(index)}
+              >
+                <Td width={'56px'}>
+                  <Avatar size={'xs'} />
+                </Td>
+                <Td>
+                  <Text>{employee.name}</Text>
+                </Td>
+                <Td>
+                  <Flex {...styles.iconBox}>
+                    <NotificationIcon number="1" />
+                    <BiRightArrowAlt
+                      size="20"
+                      visibility={
+                        hoveredRowIndex === index || index === clickedRowIndex
+                          ? 'visible'
+                          : 'hidden'
+                      }
+                    />
+                  </Flex>
+                </Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </TableContainer>
