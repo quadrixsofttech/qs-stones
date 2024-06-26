1b35b730b2ea74ac7548d8fc3c517b3d767134a7
bonderovicmartin8 <131369712+bonderovicmartin8@users.noreply.github.com>
2024-06-26 14:38:04 +0200

    [STONERS-21] Added check to not send empty dates

--- app/src/components/RemoteModal/RemoteModal.jsx
+++ app/src/components/RemoteModal/RemoteModal.jsx
@@ -36,23 +36,25 @@ export const RemoteModal = ({ isOpen, onClose }) => {
   const toast = useToast();
 
   const submitRemote = async () => {
-    await createPTO.mutateAsync({
-      dates: RemoteDates,
-      type: 'remote',
-      status: 'approved',
-      userId: user._id,
-      reviewerId: null,
-      comment: '',
-    });
-    toast({
-      title: 'Success',
-      description: 'You have scheduled work from home',
-      position: 'top-right',
-      status: 'success',
-      isClosable: true,
-      colorScheme: 'green',
-      variant: 'subtle',
-    });
+    if (RemoteDates.length >= 1) {
+      await createPTO.mutateAsync({
+        dates: RemoteDates,
+        type: 'remote',
+        status: 'approved',
+        userId: user._id,
+        reviewerId: null,
+        comment: '',
+      });
+      toast({
+        title: 'Success',
+        description: 'You have scheduled work from home',
+        position: 'top-right',
+        status: 'success',
+        isClosable: true,
+        colorScheme: 'green',
+        variant: 'subtle',
+      });
+    }
     onClose();
   };
 
