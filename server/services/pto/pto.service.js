d36c0fa27c46d75442be7c4ed880dd8bb0d6b391
ivan-srejic <ivan.srejic@quadrixsoft.com>
2024-07-08 10:08:51 +0200

    Changed recepient email for testing

--- server/services/pto/pto.service.js
+++ server/services/pto/pto.service.js
@@ -101,7 +101,8 @@ const createPTO = async ({
       });
 
       await pto.save();
-      if(type != 'remote') await sendEmail(adminEmails, `Request for ${type}`, `${user.firstName} ${user.lastName} has sent you a request for ${type}  http://stones.examia.io/admin`);
+      //Change recepients email to adminEmails when testing is over
+      if(type != 'remote') await sendEmail('katarina.kujundzic@quadrixsoft.com', `Request for ${type}`, `${user.firstName} ${user.lastName} has sent you a request for ${type}  http://stones.examia.io/admin`);
       return pto;
     } else if (type === 'vacation') {
       const user = await User.findById(userId);
@@ -136,7 +137,8 @@ const createPTO = async ({
           comment,
         });
         await pto.save();
-        await sendEmail(adminEmails, `Request for ${type}`, `${user.firstName} ${user.lastName} has sent you a request for ${type}   http://stones.examia.io/admin`);
+        //Change recepients email to adminEmails when testing is over
+        await sendEmail('katarina.kujundzic@quadrixsoft.com', `Request for ${type}`, `${user.firstName} ${user.lastName} has sent you a request for ${type}   http://stones.examia.io/admin`);
         return pto;
       } else {
         throw new Error({
