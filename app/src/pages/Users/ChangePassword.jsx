b21700f8e3f671417cf42d63f2549350035d9755
ivan-srejic <ivan.srejic@quadrixsoft.com>
2024-08-30 16:26:56 +0200

    [STONES-66] Label renamed

--- app/src/pages/Users/ChangePassword.jsx
+++ app/src/pages/Users/ChangePassword.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import SignupLayout from '../../layout/SignupLayout/SignupLayout';
-import { Link, Navigate } from 'react-router-dom';
-import { Field, Form, Formik } from 'formik';
-import * as Yup from 'yup';
+import { useState } from "react";
+import SignupLayout from "../../layout/SignupLayout/SignupLayout";
+import { Link, Navigate } from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 import {
   Alert,
   AlertIcon,
@@ -12,45 +12,45 @@ import {
   FormLabel,
   Input,
   Stack,
-} from '@chakra-ui/react';
-import useUser from '../../hooks/useUser';
+} from "@chakra-ui/react";
+import useUser from "../../hooks/useUser";
 
 const ChangePasswordSchema = Yup.object().shape({
-  oldPassword: Yup.string().required('You must type in your old password'),
-  newPassword: Yup.string().required('You must type in your new password'),
+  oldPassword: Yup.string().required("You must type in your old password"),
+  newPassword: Yup.string().required("You must type in your new password"),
 });
 
 const ChangePassword = () => {
   const [redirectOnLogin, setRedirectOnLogin] = useState(false);
   const [successfullPasswordChange, setSuccessfullPasswordChange] =
-    useState('');
+    useState("");
   const [passwordChangeError, setPasswordChangeError] = useState(false);
   const { changePassword } = useUser();
 
   const handleSubmitCredientals = async (credentials) => {
     try {
       await changePassword.mutateAsync(credentials);
-      setSuccessfullPasswordChange('Password changed successfully');
+      setSuccessfullPasswordChange("Password changed successfully");
       setPasswordChangeError(null);
       setTimeout(() => {
         setRedirectOnLogin(!redirectOnLogin);
       }, 700);
     } catch (error) {
       setPasswordChangeError(
-        error.response?.data?.message || 'Failed to change password'
+        error.response?.data?.message || "Failed to change password"
       );
-      setSuccessfullPasswordChange('');
+      setSuccessfullPasswordChange("");
     }
   };
 
   return (
     <>
-      {redirectOnLogin && <Navigate to={'/dashboard'} />}
+      {redirectOnLogin && <Navigate to={"/dashboard"} />}
       <SignupLayout title="Change password">
         <Formik
           initialValues={{
-            oldPassword: '',
-            newPassword: '',
+            oldPassword: "",
+            newPassword: "",
           }}
           onSubmit={(values) => handleSubmitCredientals(values)}
           validationSchema={ChangePasswordSchema}
@@ -100,7 +100,7 @@ const ChangePassword = () => {
                 </FormControl>
                 <Stack>
                   <Button colorScheme="blue" type="submit">
-                    Sign Up
+                    Change password
                   </Button>
                   <Button type="submit" as={Link} to="/dashboard">
                     Go back
